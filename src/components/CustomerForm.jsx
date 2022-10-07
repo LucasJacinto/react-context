1b0f6fa15ef45ea25eaad93aafb930095123ef47
Lucas Jacinto <lucaslimamelo98@gmail.com>
2022-10-07 12:04:35 -0300

    React Context

---
+++ src/components/CustomerForm.jsx
@@ -0,0 +1,46 @@
+import { useState, useContext } from 'react'
+
+import { CustomerContext } from '../contexts/customer'
+
+import './CustomerForm.css'
+
+const CustomerForm = () => {
+    const { submit } = useContext(CustomerContext)
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('dados componente', { name, email })
+
+        submit({ name, email })
+    }
+
+    return (
+        <form className="form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Nome:</label>
+            <input 
+                type="text" 
+                name="name" 
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+
+            <label htmlFor="email">Email:</label>
+            <input 
+                type="email"
+                name="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <button type='submit'>Enviar</button>
+        </form>
+    )
+}
+
+export default CustomerForm
